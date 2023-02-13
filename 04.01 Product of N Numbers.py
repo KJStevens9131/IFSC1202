29ed67b97ef97e5965eb6010db1d9d05a1ade044
KJStevens9131 <kjstevens@ualr.edu>
2023-02-13 16:42:04 +0000

    assignments

---
+++ 04.01 Product of N Numbers.py
@@ -0,0 +1,8 @@
+n = int(input("Enter N: "))
+num = 0
+result = 1
+for i in range(1, n + 1):
+    num = int(input("Enter A Number: "))
+    result *= num
+print(result)
+
---
+++ 04.04 Number of Zeros.py
@@ -0,0 +1,8 @@
+n = int(input("Enter N: "))
+num = 0
+result = 1
+for i in range(1, n + 1):
+    num = int(input("Enter A Number: "))
+    if num == 0:
+        print(result + 1)
+#print(result)
