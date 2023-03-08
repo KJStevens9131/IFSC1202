7168185f672a1514cafa104f36025927533709a3
KJStevens9131 <kjstevens@ualr.edu>
2023-03-08 16:02:58 +0000

    s

--- 07.03 Two Halves.py
+++ 07.03 Two Halves.py
@@ -1,7 +1,5 @@
-a = input()
-for i in range(len(a)//2,len(a)):  # iterating from the half of the string to last 
-    # this will for both even and odd because a//b will give half for even and ceiling for odd
-    # in both the program will be correct as half will be in second string only
+a = input("Enter a string: ")
+for i in range(len(a)//2,len(a)):  
     print(a[i],end="")
-for i in range(len(a)//2): # now printing the second half
+for i in range(len(a)//2):
     print(a[i],end="")
